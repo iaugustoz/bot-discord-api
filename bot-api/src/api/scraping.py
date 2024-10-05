c9529d9909404c311d44658796256860978b2eb0
Pedro Henrique do Prado <pedro.phprado@gmail.com>
2024-10-04 23:35:16 -0300

    :sparkles: feat: extraindo dados dos produtos

--- bot-api/src/api/scraping.py
+++ bot-api/src/api/scraping.py
@@ -6,31 +6,65 @@ import time
 
 
 
-def parser_selenium(url):
+def parser_kabum(url):
+
+    product = {}
+
+    lista_produtos = []
+
+
     try:
+
         # Inicializa o ChromeDriver
         service = Service(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service)
 
         driver.get(url)
-        time.sleep(5)
+        time.sleep(10)
 
         # Localiza a div pai
         promo_div = driver.find_element(By.CLASS_NAME, 'swiper-wrapper')
-
         if promo_div:
-            # Pega todos os links
-            produtos = promo_div.find_elements(By.CLASS_NAME, 'productLink')
+            # Pega todos os produtos
+            produtos = promo_div.find_elements(By.CLASS_NAME, 'productCard')
 
             for produto in produtos:
-                href = produto.get_attribute('href')
-                if href:
-                    print(href)
-        else:
-            print("Não foi possível localizar a div com a classe swiper-wrapper.")
+
+                product = {}
+
+                # Pega o link
+                link_element = produto.find_element(By.CLASS_NAME, 'productLink')
+                if link_element:
+                    href = link_element.get_attribute('href')
+                    product['link'] = href
+
+                # Pega a imagem
+                img_element = produto.find_element(By.CLASS_NAME, 'imageCard')
+                if img_element:
+                    src = img_element.get_attribute('src')
+                    product['image'] = src
+
+                try:
+                    name_element = produto.find_element(By.CLASS_NAME, 'bESAoj')
+                    span_element = name_element.find_element(By.TAG_NAME, 'span')
+                    product['name'] = span_element.text
+                except:
+                    product['name'] = 'Nome indisponível'
+
+                try:
+                    price_element = produto.find_element(By.CLASS_NAME, 'availablePricesCard')
+                    span_element = price_element.find_element(By.CLASS_NAME, 'priceCard')
+                    product['price'] = span_element.text
+                except:
+                    product['price'] = 'Preço indisponível'
+
+                lista_produtos.append(product)
+
+        print(lista_produtos)
 
         # Fecha o navegador após o scraping
         driver.quit()
 
     except Exception as e:
         print("Falha ao usar Selenium:", e)
+
